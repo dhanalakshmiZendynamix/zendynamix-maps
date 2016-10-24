8c2a333a87f40c9e633dee1dc721d4c9c2154688
dhanalakshmi <dhanalakshmi@zendynamix.com>
2016-10-24 13:42:09 +0530

    added ployline to map

--- zendynamix-maps/js/controller/mapController.js
+++ zendynamix-maps/js/controller/mapController.js
@@ -20,12 +20,12 @@ zendynamixMap.controller('mapController', function ($scope,leafLetMapService) {
             leafLetMapService.drawPolygon(polyarray1,'red','green',0.3)
             leafLetMapService.setCenterLocationOfMap(25.277132 ,55.418959,15)
             // create a red polyline from an array of LatLng points
-            var latLngs=[
+          /*  var latLngs=[
                 {lat:-122.68,lng: 45.51},
                 {lat:-122.43,lng:  37.77},
                 {lat:-118.2,lng: 34.04}
             ]
-            leafLetMapService.drawPolyLineFromArrayOFLatLanObjects(latLngs,'red');
+            leafLetMapService.drawPolyLineFromArrayOFLatLanObjects(latLngs,'red');*/
 
           /*  leafLetMapService.clearAndInstantiateMap("dashBoardMap",25.248354,55.352544,18)*/
 
